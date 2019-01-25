68a8e722bba6a3b6717ce9ac8692be6904a4e63f
JanPhilippSchwefer <38353072+JanPhilippSchwefer@users.noreply.github.com>
2019-01-25 23:57:00 +0100

    Uhr geht jetzt

--- Master/Assets/Scripts/UI/Clock.cs
+++ Master/Assets/Scripts/UI/Clock.cs
@@ -1,40 +1,60 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
     public GameObject MinuteHand;
     public GameObject HourHand;
-    public CurrentTime currentTime;
-    public float Speed;
+    public CurrentTime Current;
+    public float FiveMinutes;
 
-    private float time;
     private Quaternion minuteStartRot;
     private Quaternion hourStartRot;
 
     private void Start()
     {
-        currentTime = new CurrentTime(2, 0);
-        minuteStartRot = Quaternion.identity;
-        hourStartRot = Quaternion.identity;
+        Current = new CurrentTime(2, 0);
+        MinuteHand.transform.rotation = Quaternion.Euler(0, 0, -Current.CurrentMinute * 6);
+        HourHand.transform.rotation = Quaternion.Euler(0, 0, -Current.CurrentHour * 30 - Current.CurrentMinute * 0.5f);
+        minuteStartRot = MinuteHand.transform.rotation;
+        hourStartRot = HourHand.transform.rotation;
         StartCoroutine(Hour());
     }
 
     private IEnumerator Hour()
     {
-        float targetZ = minuteStartRot.z - 5;
-        Quaternion minuteTargetRot = Quaternion.Euler(0, 0, targetZ);
-        targetZ = hourStartRot.z - 5 / 12;
-        Quaternion hourTargetRot = Quaternion.Euler(0, 0, targetZ);
+        float minuteTargetZ = MinuteHand.transform.eulerAngles.z - 30;
+        float hourTargetZ = HourHand.transform.eulerAngles.z - 30f / 12;
+        Quaternion minuteTargetRot = Quaternion.Euler(0, 0, minuteTargetZ);
+        Quaternion hourTargetRot = Quaternion.Euler(0, 0, hourTargetZ);
+
         float progress = 0;
         float current = 0;
 
         while (progress < 1)
         {
             current += Time.deltaTime;
-            progress = current / 12;
+            progress = current / FiveMinutes;
+            MinuteHand.transform.rotation = Quaternion.Slerp(minuteStartRot, minuteTargetRot, progress);
             HourHand.transform.rotation = Quaternion.Slerp(hourStartRot, hourTargetRot, progress);
             yield return null;
         }
+
+        minuteStartRot = minuteTargetRot;
+        hourStartRot = hourTargetRot;
+        Current.CurrentMinute += 5;
+
+        if (Current.CurrentMinute == 60)
+        {
+            Current.CurrentHour++;
+            Current.CurrentMinute = 0;
+        }
+
+        if (Current.CurrentHour == 12)
+            Current.CurrentHour = 0;
+
+        Debug.Log(Current.CurrentHour + ":" + Current.CurrentMinute);
+
+        yield return StartCoroutine(Hour());
     }
 }
