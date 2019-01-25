10ceddeb770ecda93284c6a10b62b7f07590b72b
Kevin <38255521+Snowball115@users.noreply.github.com>
2019-01-26 00:30:06 +0100

    zeug

---
+++ Master/Assets/Scripts/Collectible.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private GameObject player;
+    private Collider col;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+        col = player.GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.Equals(col))
+        {
+            PickUp();
+        }
+    }
+
+    private void PickUp()
+    {
+        Destroy(gameObject);
+    }
+}
