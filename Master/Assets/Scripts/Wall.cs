65ea0cf09ce1920ccd267bef67048669dc7bbcd0
Kevin <38255521+Snowball115@users.noreply.github.com>
2019-01-27 04:21:15 +0100

    alter stand vlt wieder da

--- Master/Assets/Scripts/Wall.cs
+++ Master/Assets/Scripts/Wall.cs
@@ -5,7 +5,6 @@ public class Wall : MonoBehaviour
 {
     private MeshFilter filter;
     private Mesh mesh;
-    public GameObject[] disableAditionally;
 
     private void Awake()
     {
@@ -16,14 +15,10 @@ public class Wall : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         filter.mesh = null;
-        foreach (var o in disableAditionally)
-            o.SetActive(false);
-}
+    }
 
     private void OnTriggerExit(Collider other)
     {
         filter.mesh = mesh;
-        foreach (var o in disableAditionally)
-            o.SetActive(true);
     }
 }
