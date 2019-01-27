4ac318623d305bee6caba6c4bcffd910b133d195
Kevin <38255521+Snowball115@users.noreply.github.com>
2019-01-27 15:03:16 +0100

    Merge branch 'master' of https://github.com/rausc-daniel/Home

---
+++ Master/Assets/PlaySound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        Debug.Log(audio);
+    }
+    void OnEnable()
+    {
+        //audio.Play(0);
+    }
+}
