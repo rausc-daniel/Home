6d6b65208fd3a37fa68602ed097a6d78c57d5c90
Daniel Rauschenberger <38142517+rausc-daniel@users.noreply.github.com>
2019-01-27 13:50:30 +0100

    Gewinnen

---
+++ Master/Assets/Win.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    public GameObject WinScreen;
+
+    private void Start()
+    {
+        WinScreen.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerController>())
+            WinScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
