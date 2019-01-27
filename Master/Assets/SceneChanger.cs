c4bfaa0addedeccbcbcd68a61b1384c5d12b2a2b
Daniel Rauschenberger <38142517+rausc-daniel@users.noreply.github.com>
2019-01-27 16:06:00 +0100

    intro scene

---
+++ Master/Assets/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void GoToIntroScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void GoToCreditScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Master/Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class test : MonoBehaviour
+{
+    public VideoPlayer player;
+    public RawImage image;
+    RenderTexture text;
+
+    void Start()
+    {
+        text = new RenderTexture((int)player.clip.width, (int)player.clip.height, 0);
+
+        player.targetTexture = text;
+        image.texture = text;
+
+        Vector3 scale = image.transform.localScale;
+
+        //scale.y = player.clip.height / (float)player.clip.width * scale.y;
+
+        image.transform.localScale = scale;
+    }
+}
